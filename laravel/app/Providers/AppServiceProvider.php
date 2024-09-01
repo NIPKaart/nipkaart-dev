5819147bfba22a8af2aef58aeaf765ea5a973eba
Klaas Schoute <hello@student-techlife.com>
2024-09-02 00:46:17 +0200

    Add filament panel switch

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Role;
 use App\Policies\RolePolicy;
+use BezhanSalleh\PanelSwitch\PanelSwitch;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,5 +24,25 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::policy(Role::class, RolePolicy::class);
+
+        PanelSwitch::configureUsing(function (PanelSwitch $panelSwitch) {
+            $panelSwitch
+                // ->simple()
+                ->modalWidth('sm')
+                ->modalHeading('Switch between panels')
+                ->iconSize(24)
+                ->icons([
+                    'admin' => 'heroicon-o-building-office',
+                    'app' => 'heroicon-o-user',
+                ])
+                ->labels([
+                    'app' => 'User Panel',
+                    'admin' => 'Admin Panel',
+                ])
+                ->visible(fn (): bool => auth()->user()?->hasAnyRole([
+                    'admin',
+                    'manager',
+                ]));
+        });
     }
 }
