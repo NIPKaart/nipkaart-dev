c63034e17ec12852ecd40cf369de7c37dc320bb9
Klaas Schoute <hello@student-techlife.com>
2024-09-01 12:56:26 +0200

    Add Province model with migration and seeder (#52)
    * Rename the user seeder
    
    * Add Province model with migration and seeder

--- laravel/app/Models/Country.php
+++ laravel/app/Models/Country.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
@@ -17,4 +18,12 @@ class Country extends Model
     protected $fillable = [
         'name', 'code',
     ];
+
+    /**
+     * Get the provinces for the country.
+     */
+    public function provinces(): HasMany
+    {
+        return $this->hasMany(Province::class, 'country_id');
+    }
 }
