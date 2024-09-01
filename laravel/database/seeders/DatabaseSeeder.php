da088444c6bc5a2da76900a214356cf6ca2fa16b
Klaas Schoute <hello@student-techlife.com>
2024-09-01 12:26:52 +0200

    Add Country model with migration and seeder (#51)

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ShieldSeeder::class,
+            CountrySeeder::class,
             UserTableSeeder::class,
         ]);
     }
